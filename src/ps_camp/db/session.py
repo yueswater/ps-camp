d50b136c09fb94cf883daf3f60f48e210e923ac7
xiaolong70701 <xiaolong70701@gmail.com>
2025-06-22 16:49:48 +0800

    modify session

--- src/ps_camp/db/session.py
+++ src/ps_camp/db/session.py
@@ -1,8 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Database connection URL (SQLite here, can change to PostgreSQL, MySQL, etc.)
-DATABASE_URL = "sqlite:///src/ps_camp/db/camp.db"
+# DATABASE_URL = "sqlite:///src/ps_camp/db/camp.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///src/ps_camp/db/camp.db")
 
 # Create engine
 engine = create_engine(DATABASE_URL, echo=True, future=True)
